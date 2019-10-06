526f339ad3e92ebfbdad9404c7bb082b355d754c
freeyin <42679593+freeyin@users.noreply.github.com>
2019-10-06 11:15:00 +0800

    javcom

--- jiedian/index.php
+++ jiedian/index.php
@@ -1,22 +1,26 @@
 <script language="javascript">
 function dogo(){
 var urls = new Array();
-urls[0]="https://nameless-snowflake-6efa.freevpn1.workers.dev";
-urls[1]="https://lucky-disk-a08d.freevpn2.workers.dev/";
-urls[2]="https://throbbing-cherry-41d8.freevpn3.workers.dev";
-urls[3]="https://spring-band-58cb.freevpn4.workers.dev";
-urls[4]="https://delicate-rice-fb92.freevpn5.workers.dev/";
-urls[5]="https://long-forest-6e39.freevpn6.workers.dev"
-urls[6]="https://dry-bird-2b08.freevpn7.workers.dev/"
-urls[7]="https://polished-rice-0f80.freevpn8.workers.dev/"
-urls[8]="https://winter-truth-48b3.freevpn9.workers.dev/"
-urls[9]="https://weathered-silence-a13f.freevpn10.workers.dev/"
-urls[10]="https://jolly-salad-96e5.freevpn11.workers.dev/"
-urls[11]="https://silent-union-d16d.freevpn12.workers.dev/"
-urls[12]="https://wandering-haze-0e6b.freevpn14.workers.dev"
-urls[13]="https://hidden-glitter-bf78.pan666.workers.dev/"
-urls[14]="https://solitary-tooth-08ea.6443336505665416464fguyf.workers.dev/"
-n = Math.floor(Math.random()*15);
+urls[0]="http://eunsetee.com/O2fL";
+urls[1]="http://eunsetee.com/O2fb";
+urls[2]="http://eunsetee.com/O2fp";
+urls[3]="http://eunsetee.com/O2g2";
+urls[4]="http://eunsetee.com/O2gp";
+urls[5]="http://eunsetee.com/O2h1"
+urls[6]="http://eunsetee.com/O2hI"
+urls[7]="http://eunsetee.com/O2iH"
+urls[8]="http://eunsetee.com/O2ib"
+urls[9]="http://eunsetee.com/O2in"
+urls[10]="http://eunsetee.com/O2j5"
+urls[11]="http://eunsetee.com/O2jI"
+urls[12]="http://eunsetee.com/O2jW"
+urls[13]="http://eunsetee.com/O2je"
+urls[14]="http://eunsetee.com/O2jx"
+urls[15]="http://eunsetee.com/O2kK"
+urls[16]="http://eunsetee.com/O2ka"
+urls[17]="http://eunsetee.com/O2l2"
+urls[18]="http://eunsetee.com/O2lN"
+n = Math.floor(Math.random()*19);
 location.href= urls[n];
 }
 </script>
